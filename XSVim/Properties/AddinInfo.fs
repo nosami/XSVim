1efac17ed32e7366370ba158268a52e50b1e8bbc
jason <jason@jasons-MacBook-Pro.local>
2016-12-17 23:53:00 +0000

    Add solution

---
+++ XSVim/Properties/AddinInfo.fs
@@ -0,0 +1,17 @@
+﻿namespace XSVim
+
+open System
+open Mono.Addins
+open Mono.Addins.Description
+
+[<assembly:Addin (
+  "XSVim", 
+  Namespace = "XSVim",
+  Version = "1.0"
+)>]
+
+[<assembly:AddinName ("XSVim")>]
+[<assembly:AddinCategory ("IDE extensions")>]
+[<assembly:AddinDescription ("XSVim")>]
+[<assembly:AddinAuthor ("jason")>]
+()
