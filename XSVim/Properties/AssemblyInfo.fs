710a6f64eaef12235941ef024bd130cda99ba992
nosami <jasonimison@gmail.com>
2018-06-11 15:57:45 +0100

    Fix build info version. Fixes #228

--- XSVim/Properties/AssemblyInfo.fs
+++ XSVim/Properties/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Runtime.CompilerServices
 [<AutoOpen>]
 module AddinVersion =
     [<Literal>]
-    let version = "0.60.4"
+    let version = "0.60.5"
 
 [<assembly: AssemblyTitle("XSVim")>]
 // The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
