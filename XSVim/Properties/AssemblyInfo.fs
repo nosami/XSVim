8a1dc225f6c3cf98a78923e4495ce8c75330e369
nosami <jasonimison@gmail.com>
2018-11-22 18:44:17 +0000

    Bump to 0.64.2

--- XSVim/Properties/AssemblyInfo.fs
+++ XSVim/Properties/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Runtime.CompilerServices
 [<AutoOpen>]
 module AddinVersion =
     [<Literal>]
-    let version = "0.64.1"
+    let version = "0.64.2"
 
 [<assembly: AssemblyTitle("XSVim")>]
 // The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
