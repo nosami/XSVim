94cd95606907d531fd7995b478a06a33f966257f
nosami <jasonimison@gmail.com>
2018-12-15 19:06:09 +0000

    Fixes hang when switching branch / making large text changes
    Don't concat action list for each changed character
    
    Fixes #233

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-mono "/Applications/Visual Studio.app/Contents/Resources/lib/monodevelop/bin/vstool.exe" run-md-tests XSVim.Tests/bin/Debug/XSVim.Tests.dll
+mono "/Applications/Visual Studio.app/Contents/Resources/lib/monodevelop/bin/vstool.exe" run-md-tests XSVim.Tests/bin/Debug/XSVim.Tests.dll -labels
