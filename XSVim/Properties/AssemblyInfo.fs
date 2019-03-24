d0f8dde2f796361e6b26335fba28909bbe27490a
nosami <jasonimison@gmail.com>
2019-03-24 15:33:16 +0000

    Add Colemak & Dvorak layout handling

--- XSVim/Properties/AssemblyInfo.fs
+++ XSVim/Properties/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Runtime.CompilerServices
 [<AutoOpen>]
 module AddinVersion =
     [<Literal>]
-    let version = "0.64.6.80"
+    let version = "0.65.0.80"
 
 [<assembly: AssemblyTitle("XSVim")>]
 // The assembly version has the format {Major}.{Minor}.{Build}.{VSMacVersion}
