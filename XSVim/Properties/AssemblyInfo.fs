1a4353587176431f9636c964f8dd769e26f48444
Jason Imison <nosami@users.noreply.github.com>
2020-03-24 06:44:41 +0000

    Rename key binding scheme to avoid clash with VSVim

--- XSVim/Properties/AssemblyInfo.fs
+++ XSVim/Properties/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Runtime.CompilerServices
 [<AutoOpen>]
 module AddinVersion =
     [<Literal>]
-    let version = "0.65.11.81"
+    let version = "0.65.11.82"
 
 [<assembly: AssemblyTitle("XSVim")>]
 
